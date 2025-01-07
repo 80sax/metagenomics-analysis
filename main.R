3c12aeb30904e554873cc3f48be2f86cdfd4af76
Abraham <asoteloa@outlook.com>
2025-01-07 13:23:33 -0600

    Initial main script file

---
+++ main.R
@@ -0,0 +1,12 @@
+# ------------------------------------------------------
+# File: main.R
+# Authors: Abraham Sotelo, Ha Lam Hai
+# Date: 2025-01-07
+#
+# Description: Main script to run the full Metagenomics Analysis
+#
+# Inputs:
+#
+# Outputs:
+#
+# ------------------------------------------------------
